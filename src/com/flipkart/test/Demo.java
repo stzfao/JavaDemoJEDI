3fd7367349d2b2de042e0f2748e3156ebf0fcd23
Pragya Pandey <pragya.pandey1@fk1089-215022l.fkipl.flipkart.com>
2024-06-25 15:27:40 +0530

    start

---
+++ src/com/flipkart/test/Demo.java
@@ -0,0 +1,11 @@
+package com.flipkart.test;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
